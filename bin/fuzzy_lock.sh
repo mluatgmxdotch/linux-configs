e20653be2eebbcbb4e0305f64e8f78f965d21184
Tux <tux@localhost>
2018-09-26 18:16:18 +0200

    add fuzzy_lock

---
+++ bin/fuzzy_lock.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+# Take a screenshot
+scrot /tmp/screen_locked.png
+
+# Grayscale with a yellow tint
+convert /tmp/screen_locked.png -colorspace gray -brightness-contrast -30x10  -fill wheat -tint 120 /tmp/screen_locked.png
+
+# Pixellate it 10x
+mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
+
+# Lock screen displaying this image.
+i3lock -i /tmp/screen_locked.png
+
