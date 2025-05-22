ed128102ca7d11ddb7c0b71701f15f1d41ec44f3
tux <tux@localhost>
2025-05-22 11:18:07 +0200

    feat(i3): icons for i3blocks

--- .local/share/i3blocks/power.sh
+++ .local/share/i3blocks/power.sh
@@ -6,8 +6,8 @@ OBJ="/org/freedesktop/UPower/devices/${BLOCK_INSTANCE:-$DEFAULT}"
 STATE=$(upower -i "$OBJ" | grep 'battery-level' | cut -c 26-)
 
 # Full and short texts
-echo "$TITLE: $STATE"
-echo "$TITLE: $STATE"
+echo "$TITLE $STATE"
+echo "$TITLE $STATE"
 
 # Set urgent flag below 5% or use orange below 20%
 [ ${STATE,,} = "low" ] && echo "#FF8000"
