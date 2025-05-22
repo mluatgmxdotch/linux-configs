d3d9fd5973a8b26b2f9935195b719eabbb8a38cf
tux <tux@localhost>
2025-05-22 13:49:36 +0200

    feat(i3): normalize hidpp battery state

--- .local/share/i3blocks/power.sh
+++ .local/share/i3blocks/power.sh
@@ -5,12 +5,25 @@ DEFAULT=$(upower -e | head -n 1)
 OBJ="/org/freedesktop/UPower/devices/${BLOCK_INSTANCE:-$DEFAULT}"
 STATE=$(upower -i "$OBJ" | grep 'battery-level' | cut -c 26-)
 
+case "${STATE,,}" in
+'normal' | 'full')
+  STATE='OK'
+  ;;
+'low')
+  STATE='LOW'
+  ;;
+*)
+  STATE='UNK'
+  ;;
+esac
+
 # Full and short texts
 echo "$TITLE $STATE"
 echo "$TITLE $STATE"
 
 # Set urgent flag below 5% or use orange below 20%
-[ ${STATE,,} = "low" ] && echo "#FF8000"
+[ $STATE = "LOW" ] && echo "#FF8000"
+[ $STATE = "UNK" ] && echo "#808080"
 # ${BAT%?} -le 20 ] && echo "#FF8000"
 
 exit 0
